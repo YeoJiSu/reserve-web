b1ad27d0ce092503afa78579909c8680fac35ade
Kyungseo-Park <troublesome.dev@gmail.com>
2021-07-28 00:41:15 +0900

    add header styled-components

--- components/Layout/HeaderComponent/Header.Presenter.tsx
+++ components/Layout/HeaderComponent/Header.Presenter.tsx
@@ -2,10 +2,23 @@ import React from 'react';
 import { HeaderSS } from './Header.style';
 
 const HeaderPresenter = () => {
+  const isLogin = false;
+
   return (
     <>
-      <HeaderSS>
-        <span>asdsa</span>
+      <HeaderSS isLogin={isLogin}>
+        <div className={'header-top'}>
+          <div className={'header-login'}>
+            {isLogin ? <span>로그인/회원가입</span> : <span>OOO 님</span>}
+          </div>
+        </div>
+        <div className={'header-bottom'}>
+          <h1>로고</h1>
+          <ul>
+            <li>빠른 예약</li>
+            <li>승마장 보러가기</li>
+          </ul>
+        </div>
       </HeaderSS>
     </>
   );
--- components/Layout/HeaderComponent/Header.style.ts
+++ components/Layout/HeaderComponent/Header.style.ts
@@ -1,7 +1,47 @@
 import styled from 'styled-components';
 
 //styled-component 적용하기
-export const HeaderSS = styled.div`
-  height: 120px;
+export const HeaderSS = styled.div<{ isLogin }>`
+  height: 200px;
   background-color: ${(props) => props.theme.red};
+
+  .header-top {
+    background-color: aquamarine;
+    text-align: right;
+
+    .header-login {
+      span {
+        border: 1px solid;
+        border-radius: 10px;
+        padding: 10px;
+        background-color: ${({ isLogin }) => (isLogin ? 'blue' : 'red')};
+        color: #ffffff;
+      }
+    }
+  }
+
+  .header-bottom {
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    justify-content: space-between;
+    flex-wrap: nowrap;
+    height: 100%;
+    background-color: aquamarine;
+
+    h1 {
+      width: 200px;
+      text-align: center;
+      margin: 0px;
+    }
+
+    ul {
+      background-color: bisque;
+      width: calc(100% - 200px);
+    }
+    li {
+      padding: 0px 30px;
+      display: inline-block;
+    }
+  }
 `;
