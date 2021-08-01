f931c6b95ce2d218cce5206c1fd3459dbc139eb6
YeoJiSu <duwltn1301@naver.com>
2021-08-02 02:29:47 +0900

    not changed much

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,9 +5,10 @@ import React from "react";
 const IndexPage = (): JSX.Element => {
   return (
     <Layout>
-      <img className="phoneImage" width="100%" alt="iPhone_01" src="bg1.jpg" />
       <main className={"styles.main"}>
-        <h1 className={"styles.title"}>승마장 예약을 한눈에 ! </h1>
+        <h1 className={"styles.title"}>
+          승마장을 예약을 한눈에 !<br />
+        </h1>
         <p className={"styles.description"}>
           <code className={"styles.code"}>pages/index.js</code>
         </p>
