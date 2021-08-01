09e7b154177d54207421dc5290715654ea4218e6
YeoJiSu <duwltn1301@naver.com>
2021-08-01 18:49:59 +0900

    add

--- components/Layout/HeaderComponent/Header.Presenter.tsx
+++ components/Layout/HeaderComponent/Header.Presenter.tsx
@@ -1,26 +1,28 @@
 import React from "react";
 import { HeaderSS } from "@/components/Layout/HeaderComponent//Header.style";
-import Logo from "@/assets/logo.svg";
+
 import LoginStateComponent from "@/components/Common/Login/LoginStateComponent";
 
 const HeaderPresenter = ({ isLogin }) => {
   return (
     <>
       <HeaderSS isLogin={isLogin}>
-        <div className="header-top">
-          <div className="header-logo">
-            <Logo width="100px" height="auto" color="red" />
-          </div>
-          <div className="header-login">
+        <div className={"header-top"}>
+          <p>
+            🦄 러브 홀스 <a>love horse</a>
+          </p>
+          <div className={"header-login"}>
             <LoginStateComponent isLogin={isLogin} />
           </div>
         </div>
         <div className="header-bottom">
           {/*<h1>ㅇㅖ약</h1>*/}
+          <hr />
           <ul>
             <li>빠른 예약</li>
-            <li>이용 안내</li>
-            <li>이잉?</li>
+            <li>승마장 찾기</li>
+            <li>문의 Q & A </li>
+            <li>About Us</li>
           </ul>
         </div>
       </HeaderSS>
