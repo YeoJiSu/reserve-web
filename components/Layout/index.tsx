0543a91703308676ec417ac1d48b93b74ad7029e
YeoJiSu <duwltn1301@naver.com>
2021-08-14 00:46:45 +0900

    delete BodyComponent

--- components/Layout/index.tsx
+++ components/Layout/index.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import FooterContainer from "./FooterComponent/Footer.Container";
 import HeaderContainer from "./HeaderComponent/Header.Container";
-import BodyContainer from "./BodyComponent/Body.Container";
+
 
 interface Props {
   children?: React.ReactNode;
@@ -10,7 +10,6 @@ const Layout = ({ children }: Props): JSX.Element => {
   return (
     <>
       <HeaderContainer />
-      <BodyContainer />
       <span>{children}</span>
       <FooterContainer />
     </>
