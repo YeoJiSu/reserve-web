57973ad1a6a172e8a6be40a64634368944257071
YeoJiSu <duwltn1301@naver.com>
2021-08-14 00:47:45 +0900

    add BodyComponent module

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,11 @@
 import Layout from "components/Layout";
 import React from "react";
 // import Vercel from '../public/vercel.svg';
-
+import BodyContainer from "../pages/BodyComponent/Body.Container";
 const IndexPage = (): JSX.Element => {
   return (
     <Layout>
+      <BodyContainer/>
       <main className={"styles.main"}>
         <h1 className={"styles.title"}>
           
