7b3ef0e874f2c7c38efe057b1bbc8555f1dd3a81
Kyungseo-Park <troublesome.dev@gmail.com>
2021-08-17 23:54:36 +0900

    move login container & presenter

--- controllers/AuthController/LoginController/Login.Container.tsx
+++ controllers/AuthController/LoginController/Login.Container.tsx
@@ -1,11 +1,45 @@
-import React from "react";
+import React, { useRef, useCallback } from "react";
+import { useDispatch } from "react-redux";
 import LoginPresenter from "./Login.Presenter";
+import { loginAPI } from "lib/api/auth";
+import { userType } from "@/types/userType";
+import { userAcions } from "@/store/user";
+import useInput from "hooks/useInput";
+const LoginContainer: React.FC = (): JSX.Element => {
+  // Input Hooks
+  const [identifier, onChangeIdentifier] = useInput("");
+  const [password, onChangePassword] = useInput("");
 
-const LoginContainer: React.FC = () => {
+  // 비밀번호 이슈 발생 시 focus를 맞춘다. => ref HTML 객체 다룰때 사용함.
+  const passwordInput = useRef<HTMLInputElement>(null);
+  const dispatch = useDispatch();
+
+  // 로그인 이벤트
+  const onSubmitLogIn = useCallback(
+    async (e) => {
+      e.preventDefault();
+      try {
+        const { data }: { data: userType } = await loginAPI({
+          identifier,
+          password,
+        });
+        dispatch(userAcions.setLoggedUser(data));
+      } catch (e) {
+        console.log(e);
+      }
+    },
+    [identifier, password, dispatch],
+  );
+
   return (
-    <>
-      <LoginPresenter />
-    </>
+    <LoginPresenter
+      onSubmitLogIn={onSubmitLogIn}
+      identifier={identifier}
+      onChangeIdentifier={onChangeIdentifier}
+      passwordInput={passwordInput}
+      onChangePassword={onChangePassword}
+      password={password}
+    />
   );
 };
 
--- controllers/AuthController/LoginController/Login.Presenter.tsx
+++ controllers/AuthController/LoginController/Login.Presenter.tsx
@@ -1,7 +1,72 @@
 import React from "react";
+import Link from "next/link";
+import {
+  LocalSignupBox,
+  LoginContainer,
+  LoginFormBox,
+  LoginHeader,
+  LoginWrapper,
+} from "./Login.Style";
+interface LoginProps {
+  onSubmitLogIn: (e) => void;
+  identifier: string;
+  password: string;
+  onChangeIdentifier: (e) => void;
+  passwordInput: React.MutableRefObject<HTMLInputElement>;
+  onChangePassword: (e) => void;
+}
 
-const LoginPresenter = (): JSX.Element => {
-  return <></>;
+const LoginPresenter = ({
+  onSubmitLogIn,
+  identifier,
+  onChangeIdentifier,
+  passwordInput,
+  onChangePassword,
+  password,
+}: LoginProps): JSX.Element => {
+  return (
+    <>
+      <LoginContainer>
+        <LoginWrapper>
+          <LoginHeader>
+            <h1>로그인</h1>
+            <p>
+              {`${process.env.SERVICE_NAME}의 다양한 혜택을 ${process.env.SERVICE_NAME} 회원가입을 통해 편리하게 이용하세요.`}
+            </p>
+          </LoginHeader>
+          <LoginFormBox onSubmit={onSubmitLogIn}>
+            {/* 컴포넌트화가 필요함 */}
+            <div>
+              <label></label>
+              <input
+                type="text"
+                value={identifier}
+                placeholder="이메일을 입력해주세요."
+                onChange={onChangeIdentifier}
+              />
+            </div>
+            <div>
+              <label></label>
+              <input
+                type="password"
+                ref={passwordInput}
+                value={password}
+                placeholder="비밀번호를 입력해주세요."
+                onChange={onChangePassword}
+              />
+            </div>
+            <button type="submit">로그인</button>
+          </LoginFormBox>
+          <LocalSignupBox>
+            <p>아직 회원이 아니신가요?</p>
+            <div>
+              <Link href="/auth/register">회원가입</Link>
+            </div>
+          </LocalSignupBox>
+        </LoginWrapper>
+      </LoginContainer>
+    </>
+  );
 };
 
 export default LoginPresenter;
